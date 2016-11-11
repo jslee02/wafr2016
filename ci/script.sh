7182dc03bdf39dc4d259bbab4d8f2990f478fe45
Jeongseok Lee <jslee02@users.noreply.github.com>
2016-11-11 10:35:31 -0500

    Update script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$COMPILER" = "CLANG" ]; then exit;
 
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDART_TREAT_WARNINGS_AS_ERRORS=ON -DDART_COVERALLS=$COVERALLS ..
-make -j4 tutorials examples tests
-if [ $COVERALLS = ON ]; then make -j4 coveralls; else make -j4 test; fi
-
+#make -j4 tutorials examples tests
+#if [ $COVERALLS = ON ]; then make -j4 coveralls; else make -j4 test; fi
+make -j4 experiments examples tests
+make -j4 test
